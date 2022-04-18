bb5cb46d260828f1055e982fdb7d60a63320218d
Enoc Velasquez <fungicoder@gmail.com>
2022-04-18 13:41:23 -0600

    New controller and branch

---
+++ src/main/java/com/fordevs/jsontotextformat/controller/SimpleCitController.java
@@ -0,0 +1,12 @@
+package com.fordevs.jsontotextformat.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pharmacylevelbenefit/v1/partialintent1")
+@Slf4j
+public class SimpleCitController {
+
+}
